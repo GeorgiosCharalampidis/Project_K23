680ca3718b9924218708b2aa884b0f23423b13aa
Georgios Charalampidis <36412280+GeorgiosCharalampidis@users.noreply.github.com>
2023-10-12 02:12:21 +0300

    Testing

---
+++ cmake-build-debug/CMakeFiles/ProjectEm.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjectEm.dir/main.cpp.obj"
+  "CMakeFiles/ProjectEm.dir/main.cpp.obj.d"
+  "ProjectEm.exe"
+  "ProjectEm.exe.manifest"
+  "ProjectEm.pdb"
+  "libProjectEm.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProjectEm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
