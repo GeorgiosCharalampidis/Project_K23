29eccdab0204c83b69436a92008a5490127060b3
Georgios Charalampidis <36412280+GeorgiosCharalampidis@users.noreply.github.com>
2023-10-12 17:15:57 +0300

    Hello

--- main.cpp
+++ main.cpp
@@ -62,34 +62,89 @@ void print_image(const std::vector<unsigned char>& image, int width, int height)
     std::cout << std::endl;
 }
 
+
+// Το παρακάτω τμήμα κώδικα χρησιμοποιήθηκε για την αντιγραφή κάποιων εικόνων από το input.dat στο query.dat
+
+
+
 int main() {
     int number_of_images, image_size;
 
-    std::string mnist_file;
-    std::cout << R"(Enter the path to the MNIST file (e.g., C:\path\to\index.dat): )";
-    std::cin >> mnist_file;
-
-
-    // Προφανώς η τοποθεσία του αρχείου θα πρέπει να αλλάξει ανάλογα με τον υπολογιστή σας
-    //std::string mnist_file = R"(C:\Users\test\CLionProjects\ProjectEm\input.dat)";
-
-    // Διαβάζουμε τα δεδομένα των εικόνων MNIST από το αρχείο
-    std::vector<std::vector<unsigned char>> dataset = read_mnist_images(mnist_file, number_of_images, image_size);
-
-    // Εκτυπώνουμε όλες τις εικόνες στο dataset
-    // Ντελαπόνγκο , στο input.dat υπάρχουν 60000 εικόνες , με κάθε εικόνα να έχει 784 pixels
-    // Οπότε πρακτικά έχουμε ένα vector 60000 εικόνων , με κάθε διάνυσμα στο vector να έχει 784 διαστάσεις
-
+    // Η άσκηση ζητάει τα paths κατά την εκτέλεση του προγράμματος, που έχει υλοποιηθεί παρακάτω
+    // Για λόγους ταχύτητας και ευκολίας στο debugging, δίνω καρφωτά τις τοποθεσίες
 
     /*
 
+    std::string input;
+    std::cout << R"(Enter the path to the MNIST file (e.g., C:\path\to\index.dat): )";
+    std::cin >> input;
 
-    for (int i = 0; i < number_of_images; i++) {
-        std::cout << "Image " << i + 1 << ":" << std::endl;
+    // create vector to store dataset
+    std::vector<std::vector<unsigned char>> dataset = read_mnist_images(input, number_of_images, image_size);
+
+    std::cout << std::endl;
+
+    std::string query;
+    std::cout << R"(Enter the path to the MNIST_test file (e.g., C:\path\to\query.dat): )";
+    std::cin >> query;
+
+    std::cout << std::endl;
+
+    std::string outputPath;
+    std::cout << R"(Enter the path to the MNIST_test file (e.g., C:\path\to\output.dat): )";
+    std::cin >> outputPath;
+
+
+    // Create vector to store query_set
+    std::vector<std::vector<unsigned char>> query_set = read_mnist_images(query, number_of_images,image_size);
+
+    // Create an output file stream with the user-specified file path
+    std::ofstream outputFile(outputPath, std::ios::binary);
+
+
+
+    // Close the file
+    outputFile.close();
+
+
+
+    */
+
+
+    // Δίνω καρφωτά τις τοποθεσίες των input.dat και query.dat
+    // Δημιουργώ αντίστοιχα τα dataset και query_set
+
+
+
+    std::string input = R"(C:\Users\test\CLionProjects\Project_K23\input.dat)";
+    std::string query = R"(C:\Users\test\CLionProjects\Project_K23\query.dat)";
+    std::string outputPath = R"(C:\Users\test\CLionProjects\Project_K23\output.dat)";
+
+    std::vector<std::vector<unsigned char>> dataset = read_mnist_images(input, number_of_images, image_size);
+    std::vector<std::vector<unsigned char>> query_set = read_mnist_images(query, number_of_images,image_size);
+
+
+    std::ofstream outputFile(outputPath, std::ios::binary);
+
+    // Close the file
+    outputFile.close();
+
+
+
+
+    // Εκτυπώνουμε τις εικόνες των δύο datasets για testing
+
+    /*
+
+    for (int i = 0; i < 10; i++) {
+        std::cout << "Image " << i + 1 << "of dataset:" << std::endl;
         print_image(dataset[i], 28, 28); // Οι διαστάσεις 28x28 αντιστοιχούν στο MNIST
+        std::cout << "Image " << i + 1 << "of query_set:" << std::endl;
+        print_image(query_set[i], 28, 28); // Οι διαστάσεις 28x28 αντιστοιχούν στο MNIST
     }
 
     */
+
 
 
     return 0;
