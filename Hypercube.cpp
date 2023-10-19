3f9d6c87bcad98b814881775d23af3a0da9efe93
Georgios Charalampidis <36412280+GeorgiosCharalampidis@users.noreply.github.com>
2023-10-19 17:54:18 +0300

    Nothing makes sense anymore. Im slowly but steadily losing my mind.

--- Hypercube.cpp
+++ Hypercube.cpp
@@ -1,11 +1,42 @@
 #include "Hypercube.h"
 #include <iostream>
 #include <random>
+#include <cmath>
+#include <set>
+#include <algorithm>
+#include <limits>
 
-Hypercube::Hypercube(int dim, int projDim)
-        : dimension(dim), projectedDimension(projDim) {
-    // Initialize the random matrix
-    // ...
+// Υπολογισμός της ευκλείδιας απόστασης μεταξύ δύο διανυσμάτων
+double euclideanDistance(const std::vector<unsigned char>& dataset, const std::vector<unsigned char>& query_set) {
+    if (dataset.size() != query_set.size()) {
+        throw std::runtime_error("Vectors must have the same dimension for L2 distance calculation.");
+    }
+    double distance = 0.0;
+    for (size_t i = 0; i < dataset.size(); ++i) {
+        double diff = static_cast<double>(dataset[i]) - static_cast<double>(query_set[i]);
+        distance += diff * diff;
+    }
+    return std::sqrt(distance);
+}
+
+
+
+Hypercube::Hypercube(std::vector<std::vector<unsigned char>> dataset,
+                     const std::vector<std::vector<unsigned char>>& query,
+                     int k, int num_dimensions, int N, double R, double w)
+                    : dataset(std::move(dataset)),
+                      queryDataset(query), // This was probably an error before.
+                      k(k), num_dimensions(num_dimensions),
+                      N(N), R(R), w(w),
+                      hash_table(1 << k, -1),
+                      table_functions(createHashFunctions(k, num_dimensions))
+{
+    generator = std::mt19937(std::random_device{}());
+
+}
+
+Hypercube::~Hypercube() {
+    table_functions.clear();
 }
 
 void Hypercube::insert(const std::vector<double>& vec) {
@@ -14,6 +45,126 @@ void Hypercube::insert(const std::vector<double>& vec) {
 }
 
 std::vector<std::vector<double>> Hypercube::query(const std::vector<double>& vec, int hammingThreshold) {
-    // Implement query logic
-    // ...
+    std::vector<int> candidateIndices = probe((const std::vector<unsigned char> &) vec, hammingThreshold);
+
+    // Find the closest candidate to the query
+    // TODO: Implement the logic to calculate the distance and determine the closest data point
+
+    return {};  // Return the closest data point(s) based on the logic you implement
+}
+
+void Hypercube::buildIndex(const std::vector<std::vector<unsigned char>>& data_set) {
+    for (int i = 0; i < data_set.size(); ++i) {
+        int hash_value = hashDataPoint(calculateHiValues(data_set[i]));
+        hash_table[hash_value] = i;
+    }
+}
+
+std::vector<int> Hypercube::probe(const std::vector<unsigned char>& query_point, int maxHammingDistance) {
+    int hash_value = hashDataPoint(calculateHiValues(query_point));
+    std::set<int> candidates;
+
+    for (int distance = 1; distance <= maxHammingDistance; ++distance) {
+        for (int i = 0; i < k; ++i) {
+            int neighbor_hash = hash_value ^ (1 << i);
+            if (hash_table[neighbor_hash] != -1) {
+                candidates.insert(hash_table[neighbor_hash]);
+            }
+        }
+    }
+
+    return std::vector<int>(candidates.begin(), candidates.end());
+}
+
+int Hypercube::fi(int hi_value) {
+    return hi_value % 2;  // map even to 0, odd to 1
+}
+
+int Hypercube::hashDataPoint(const std::vector<int>& hi_values) {
+    if (hi_values.size() != k) {
+        throw std::runtime_error("Invalid number of hi values.");
+    }
+
+    int g_value = 0;
+    for (int i = 0; i < k; ++i) {
+        int hi = hi_values[i];
+        g_value |= (fi(hi) << i);  // Use fi function
+    }
+
+    return g_value;
+}
+
+std::vector<std::pair<std::vector<float>, float>> Hypercube::createHashFunctions(int k, int dim) {
+    std::vector<std::pair<std::vector<float>, float>> table_functions;
+    std::normal_distribution<float> distribution(0.0, 1.0);
+    std::uniform_real_distribution<float> offset_distribution(0.0, w);
+
+    for (int i = 0; i < k; ++i) {
+        std::vector<float> v;
+        for (int j = 0; j < dim; ++j) {
+            v.push_back(distribution(generator));
+        }
+        float t = offset_distribution(generator);
+        table_functions.push_back({v, t});
+    }
+
+    return table_functions;
+}
+
+std::vector<int> Hypercube::calculateHiValues(const std::vector<unsigned char>& data_point) {
+    if (data_point.size() != num_dimensions) {
+        throw std::runtime_error("Data point dimensions mismatch.");
+    }
+
+    std::vector<int> hi_values;
+    for (int i = 0; i < k; ++i) {
+        auto& [v, t] = table_functions[i];
+        double dot_product = 0.0;
+        for (int j = 0; j < num_dimensions; ++j) {
+            dot_product += v[j] * data_point[j];
+        }
+        int hi = static_cast<int>(std::floor((dot_product + t) / w));
+        hi += 100000;
+        hi_values.push_back(hi);
+    }
+
+    return hi_values;
+}
+
+std::vector<std::vector<unsigned char>> Hypercube::kNearestNeighbors(const std::vector<unsigned char>& q, int N) {
+    std::vector<int> candidateIndices = probe(q, k);
+    std::vector<std::pair<double, std::vector<unsigned char>>> distanceAndPoints;
+    for (const auto& index : candidateIndices) {
+        double distance = euclideanDistance(dataset[index], q);
+        distanceAndPoints.push_back({distance, dataset[index]});
+    }
+
+    std::sort(distanceAndPoints.begin(), distanceAndPoints.end());
+
+    std::vector<std::vector<unsigned char>> kNearest;
+    for (int i = 0; i < std::min(N, static_cast<int>(distanceAndPoints.size())); ++i) {
+        kNearest.push_back(distanceAndPoints[i].second);
+    }
+
+    return kNearest;
+}
+
+std::vector<std::vector<unsigned char>> Hypercube::rangeSearch(const std::vector<unsigned char>& q, double R) {
+    std::vector<int> candidateIndices = probe(q, k);
+    std::vector<std::vector<unsigned char>> inRange;
+    for (const auto& index : candidateIndices) {
+        double distance = euclideanDistance(dataset[index], q);
+        if (distance <= R) {
+            inRange.push_back(dataset[index]);
+        }
+    }
+    return inRange;
+}
+
+Hypercube::NearestNeighborsResult Hypercube::findNearest(const std::vector<unsigned char>& q, int N, double R) {
+    NearestNeighborsResult result;
+    result.closestNeighbor = kNearestNeighbors(q, 1)[0];
+    result.NNearestNeighbors = kNearestNeighbors(q, N);
+    result.withinRange = rangeSearch(q, R);
+    return result;
 }
--- Hypercube.h
+++ Hypercube.h
@@ -1,30 +1,69 @@
-//
-// Created by test on 10/18/2023.
-//
-
-#ifndef PROJECTEM_HYPERCUBE_H
-#define PROJECTEM_HYPERCUBE_H
+#ifndef HYPERCUBE_H
+#define HYPERCUBE_H
 
 #include <vector>
-#include <unordered_map>
-#include <string>
+#include <random>
+#include <set>
 
 class Hypercube {
-private:
-    int dimension;
-    int projectedDimension;
-    std::vector<std::vector<double>> randomMatrix;
-
-    // Private methods for random projection and hashing
-
 public:
-    std::unordered_map<std::string, std::vector<std::vector<double>>> hypercube;
+    Hypercube(std::vector<std::vector<unsigned char>> dataset,
+              const std::vector<std::vector<unsigned char>>& query,
+              int k, int num_dimensions, int N, double R, double w);
+    ~Hypercube();
 
-    Hypercube(int dim, int projDim);
-
+    // Inserts a data point into the hypercube
     void insert(const std::vector<double>& vec);
+
+    // Queries the hypercube with a data point
     std::vector<std::vector<double>> query(const std::vector<double>& vec, int hammingThreshold);
+
+    // Builds the index for the given dataset
+    void buildIndex(const std::vector<std::vector<unsigned char>>& data_set);
+
+
+    // Θα χρησιμοποιηθεί για την επιστροφή και των τριών μεταβλητών
+    struct NearestNeighborsResult {
+        std::vector<unsigned char> closestNeighbor;
+        std::vector<std::vector<unsigned char>> NNearestNeighbors;
+        std::vector<std::vector<unsigned char>> withinRange;
+    };
+
+    NearestNeighborsResult findNearest(const std::vector<unsigned char>& q, int N, double R);
+
+
+private:
+    // Calculates hash value for a given data point using hi values
+    int hashDataPoint(const std::vector<int>& hi_values);
+
+    // Calculates hi values for a given data point
+    std::vector<int> calculateHiValues(const std::vector<unsigned char>& data_point);
+
+    // Returns candidates by probing the hypercube for the given query point
+    std::vector<int> probe(const std::vector<unsigned char>& query_point, int maxHammingDistance);
+
+    // Defines the function to map hi values to {0, 1}
+    int fi(int hi_value);
+
+    // Creates hash functions for the hypercube
+    std::vector<std::pair<std::vector<float>, float>> createHashFunctions(int k, int dim);
+
+
+    std::vector<std::vector<unsigned char>> kNearestNeighbors(const std::vector<unsigned char>& q, int N);
+    std::vector<std::vector<unsigned char>> rangeSearch(const std::vector<unsigned char>& q, double R);
+
+
+        // Member variables
+    std::vector<std::vector<unsigned char>> dataset;
+    const std::vector<std::vector<unsigned char>>& queryDataset;
+    int k;
+    int num_dimensions;
+    int N;
+    double R;
+    double w;
+    std::vector<int> hash_table;
+    std::vector<std::pair<std::vector<float>, float>> table_functions;
+    std::mt19937 generator;
 };
 
-
-#endif //PROJECTEM_HYPERCUBE_H
+#endif // HYPERCUBE_H
